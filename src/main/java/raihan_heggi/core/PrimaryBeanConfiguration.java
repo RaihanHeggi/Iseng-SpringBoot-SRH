6ff1f6ad23492ed73fc2c510b4b672ae39ce2861
RaihanHeggi <syaraihanheggi@gmail.com>
2024-07-09 20:19:30 +0700

    07/09/2024
    Update Belajar Beans, Primary Beans, dll

--- src/main/java/raihan_heggi/core/PrimaryBeanConfiguration.java
+++ src/main/java/raihan_heggi/core/PrimaryBeanConfiguration.java
@@ -1,28 +1,25 @@
 package raihan_heggi.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Bean;
 
-import lombok.extern.slf4j.Slf4j;
 import raihan_heggi.core.data.Foo;
 
-@Slf4j
 @Configuration
-public class BeanConfiguration {
+public class PrimaryBeanConfiguration {
 
     @Primary
-    @Bean
-    public Foo foo1() {
+    @Bean(name = "fooFirst")
+    public Foo foo_1() {
         Foo foo = new Foo();
-        log.info("Creating Foo 1");
         return foo;
     }
 
-    @Bean
-    public Foo foo2() {
+    @Bean(name = "fooSecond")
+    public Foo foo_2() {
         Foo foo = new Foo();
-        log.info("Creating Foo 2");
         return foo;
     }
+
 }
